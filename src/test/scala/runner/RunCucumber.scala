587c825599338d8609207f2515e7b8bf0be5718c
Henter Liu <ji.liu01@sap.com>
2018-10-24 18:04:18 +0800

    skip some file from scoverage add an empty feature file

--- src/test/scala/runner/RunCucumber.scala
+++ src/test/scala/runner/RunCucumber.scala
@@ -7,6 +7,6 @@ import cucumber.api.CucumberOptions
 @RunWith(classOf[Cucumber])
 @CucumberOptions(
   plugin = Array("pretty", "html:target/cucumber-html-report", "json:target/cucumber-report.json", "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/report.html"),
-  features = Array("classpath:features/BankAccount.feature", "classpath:features/TableCount.feature"),
-  glue = Array("bank", "db"))
+  features = Array("classpath:features/BankAccount.feature", "classpath:features/TableCount.feature", "classpath:features/Test.feature"),
+  glue = Array("bank", "db", "test"))
 class RunCucumber
